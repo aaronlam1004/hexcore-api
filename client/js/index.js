8089f8f55505eba43a1e5ba8688d6e19e3a95d60
Aaron Lam <aaronlam1004@gmail.com>
2022-06-12 23:19:29 -0700

    Updated client changes

--- client/js/index.js
+++ client/js/index.js
@@ -21,12 +21,12 @@ function GetCurrentSet() {
  * Fills champion selector with champion names for getting champion information
  **/
 function FillChampSelectorWithNames() {
-  $("#api-champions-name").empty();
-  $("#api-champions-name").append(`<option value="" disabled selected>Name</option>`);
+  $("#api-champion-names").empty();
+  $("#api-champion-names").append(`<option value="" disabled selected>Name</option>`);
   let url = GetDataApiUrl(GetCurrentSet(), "champions");
   Fetch(url).then((res) => {
     for (let champion of res) {
-      $("#api-champions-name").append(`<option value="${champion["name"]}">${champion["name"]}</option>`);
+      $("#api-champion-names").append(`<option value="${champion["name"]}">${champion["name"]}</option>`);
     }
   });
 }
@@ -35,17 +35,17 @@ function FillChampSelectorWithNames() {
  * Fills the cost filter for getting champion information
  **/
 function FillChampFilterWithCosts() {
-  $("#api-champions-cost").empty();
+  $("#api-champion-cost").empty();
 
   for (let cost = 1; cost <= 5; cost++) {
-    $("#api-champions-cost").append(`<option value="${cost}">${cost}</option>`);
-    $("#api-champions-cost").trigger("chosen:updated");
+    $("#api-champion-cost").append(`<option value="${cost}">${cost}</option>`);
+    $("#api-champion-cost").trigger("chosen:updated");
   }
 
   if (GetCurrentSet() == "7") {
-    $("#api-champions-cost").append(`<option value="8">8</option>`);
-    $("#api-champions-cost").append(`<option value="10">10</option>`);
-    $("#api-champions-cost").trigger("chosen:updated");
+    $("#api-champion-cost").append(`<option value="8">8</option>`);
+    $("#api-champion-cost").append(`<option value="10">10</option>`);
+    $("#api-champion-cost").trigger("chosen:updated");
   }
 }
 
@@ -53,13 +53,13 @@ function FillChampFilterWithCosts() {
  * Fills the trait filter for getting champion information
  **/
 function FillChampFilterWithTraits() {
-  $("#api-champions-traits").empty();
+  $("#api-champion-traits").empty();
   let url = GetDataApiUrl(GetCurrentSet(), "traits");
   Fetch(url).then((res) => {
     for (let trait of res) {
-      $("#api-champions-traits").append(`<option value="${trait["name"]}">${trait["name"]}</option>`);
+      $("#api-champion-traits").append(`<option value="${trait["name"]}">${trait["name"]}</option>`);
     }
-    $("#api-champions-traits").trigger("chosen:updated");
+    $("#api-champion-traits").trigger("chosen:updated");
   });
 }
 
@@ -67,12 +67,12 @@ function FillChampFilterWithTraits() {
  * Fills the cost filter for getting champion information
  **/
 function FilterChampionsUsingApi() {
-  let costs = $("#api-champions-cost").chosen().val();
-  let traits = $("#api-champions-traits").chosen().val();
+  let costs = $("#api-champion-cost").chosen().val();
+  let traits = $("#api-champion-traits").chosen().val();
   let filters = { cost: costs.join(' '), traits: traits.join(' ') };
   let url = GetDataApiUrl(GetCurrentSet(), "champions", filters);
   Fetch(url).then((res) => {
-    $("#api-champions-output").text(JSON.stringify(res, undefined, 2));
+    $("#api-champion-output").text(JSON.stringify(res, undefined, 2));
     UpdateApiDemoOutput(GetDataApiUrl(GetCurrentSet(), "champions", filters));
   });
 }
@@ -82,12 +82,12 @@ function FilterChampionsUsingApi() {
  * Fills trait selector with trait names for getting trait information
  **/
 function FillTraitSelectorWithNames() {
-  $("#api-traits-name").empty();
-  $("#api-traits-name").append(`<option value="" disabled selected>Name</option>`);
+  $("#api-trait-names").empty();
+  $("#api-trait-names").append(`<option value="" disabled selected>Name</option>`);
   let url = GetDataApiUrl(GetCurrentSet(), "traits");
   Fetch(url).then((res) => {
     for (let trait of res) {
-      $("#api-traits-name").append(`<option value="${trait["name"]}">${trait["name"]}</option>`);
+      $("#api-trait-names").append(`<option value="${trait["name"]}">${trait["name"]}</option>`);
     }
   });
 }
@@ -98,16 +98,16 @@ function FillTraitSelectorWithNames() {
  * @param fillNames: whether or not to fill with item names or not
  **/
 function FillItemSelector(fillNames = true) {
-  $("#api-items-select").empty();
-  $("#api-items-select").append(`<option value="" disabled selected>Name/ID</option>`);
+  $("#api-item-select").empty();
+  $("#api-item-select").append(`<option value="" disabled selected>Name/ID</option>`);
   let url = GetDataApiUrl(GetCurrentSet(), "items");
   Fetch(url).then((res) => {
     for (let item of res) {
       if (fillNames) {
-        $("#api-items-select").append(`<option value="${item["name"]}">${item["name"]}</option>`);
+        $("#api-item-select").append(`<option value="${item["name"]}">${item["name"]}</option>`);
       }
       else {
-        $("#api-items-select").append(`<option value="${item["id"]}">${item["id"]}</option>`);
+        $("#api-item-select").append(`<option value="${item["id"]}">${item["id"]}</option>`);
       }
     }
   });
@@ -117,7 +117,7 @@ function FillItemSelector(fillNames = true) {
  * Updates item selector based on the item query selector
  **/
 function UpdateItemSelectorValues() {
-  if ($("#api-items-query").val() == "ID") {
+  if ($("#api-item-query").val() == "ID") {
     FillItemSelector(fillNames = false); // IDs
   }
   else {
@@ -129,16 +129,37 @@ function UpdateItemSelectorValues() {
  * Fills the attribute filter for getting item information
  **/
 function FillItemFilterWithAttributes() {
-  $("#api-items-attributes").empty();
+  $("#api-item-attributes").empty();
   let attributes = ATTRIBUTES["all"].concat(ATTRIBUTES[GetCurrentSet()]);
   for (let attribute of attributes) {
-    $("#api-items-attributes").append(`<option value="${attribute}">${attribute}</option>`);
+    $("#api-item-attributes").append(`<option value="${attribute}">${attribute}</option>`);
   }
-  $("#api-items-attributes").trigger("chosen:updated");
+  $("#api-item-attributes").trigger("chosen:updated");
 }
 
 // -- Image --
-//TODO: image selector updater
+/**
+ * Fills trait selector with trait names for getting trait information
+ * @param imgCategory: the category of images to fill
+ **/
+function FillImageSelector(imgCategory) {
+  $("#api-img-select").empty();
+  $("#api-img-select").append(`<option value="" disabled selected>Name</option>`);
+  imgCategory += 's';
+  let url = GetDataApiUrl(GetCurrentSet(), imgCategory.toLowerCase());
+  Fetch(url).then((res) => {
+    for (let data of res) {
+      $("#api-img-select").append(`<option value="${data["name"]}">${data["name"]}</option>`);
+    }
+  });
+}
+
+/**
+ * Updates item selector based on the champion query selector
+ **/
+function UpdateImageSelectorValues() {
+  FillImageSelector($("#api-img-query").val());
+}
 
 /**
  * Updates the result so that it is the image we selected
@@ -146,8 +167,8 @@ function FillItemFilterWithAttributes() {
 function UpdateApiImage() {
   let set = GetCurrentSet();
   var src;
-  let imgQuery = $("#api-img-text").val();
-  let imgType = $("#api-img-select").val();
+  let imgQuery = $("#api-img-select").val();
+  let imgType = $("#api-img-query").val();
   switch(imgType) {
     case "Champion":
       src = GetImageSrc(set, {champion: imgQuery});
@@ -176,23 +197,27 @@ function InitializeDemos() {
 
   // -- Champion API --
   FillChampSelectorWithNames(); // Names
-  $("#api-champions-traits").chosen({allow_single_deselect: true}); // Trait filter
+  $("#api-champion-traits").chosen({allow_single_deselect: true}); // Trait filter
   FillChampFilterWithTraits();
-  $("#api-champions-cost").chosen({allow_single_deselect: true}); // Cost filter
+  $("#api-champion-cost").chosen({allow_single_deselect: true}); // Cost filter
   FillChampFilterWithCosts();
   
   // -- Traits API --
   FillTraitSelectorWithNames(); // Names
-  $("#api-traits-types").chosen({allow_single_deselect: true}); // Trait types filter
+  $("#api-trait-types").chosen({allow_single_deselect: true}); // Trait types filter
 
   // -- Items API --
   FillItemSelector(); // Names/ID
-  $("#api-items-attributes").chosen({allow_single_deselect: true}); // Item attributes filter
+  $("#api-item-attributes").chosen({allow_single_deselect: true}); // Item attributes filter
   FillItemFilterWithAttributes();
 
   // -- Image API --
+  UpdateImageSelectorValues();
 }
 
+/**
+ * Set up the event handlers
+ **/
 function SetupEventHandlers() {
   // Sets
   $("#api-sets").change(() => {
@@ -200,150 +225,151 @@ function SetupEventHandlers() {
     FillChampSelectorWithNames(); // Names
     FillChampFilterWithCosts(); // Trait filter
     FillChampFilterWithTraits(); // Cost filter
+    $("#api-champion-output").text(''); // Reset champion output
 
     // Traits
     FillTraitSelectorWithNames();
+    $("#api-trait-output").text(''); // Reset trait output
 
     // Items
     UpdateItemSelectorValues(); // Names/IDs
     FillItemFilterWithAttributes(); // Item attributes filter
+    $("#api-item-output").text(''); // Reset item output
+
+    // Images
+    UpdateImageSelectorValues(); // Names (champions, items, traits, etc.)
+    $("#api-img-result").attr("src", ''); // Reset image output
   });
   
   // -- Champion API --
   // When champion is selected
-  $("#api-champions-name").change((event) => {
-    let championName = $("#api-champions-name").val();
+  $("#api-champion-names").change((event) => {
+    let championName = $("#api-champion-names").val();
     let url = GetDataApiUrl(GetCurrentSet(), "champions", {"name": championName});
     Fetch(url).then((res) => {
-      $("#api-champions-output").text(JSON.stringify(res, undefined, 2));
+      $("#api-champion-output").text(JSON.stringify(res, undefined, 2));
       UpdateApiDemoOutput(url);
+      $("#api-champion-cost").val('').trigger("chosen:updated"); // Clear filtered costs
+      $("#api-champion-traits").val('').trigger("chosen:updated"); // Clear filtered traits
     });
   });
 
   // When "All" button is clicked
-  $("#api-champions-all").click(() => {
+  $("#api-champion-all").click(() => {
     let url = GetDataApiUrl(GetCurrentSet(), "champions");
     Fetch(url).then((res) => {
-      $("#api-champions-output").text(JSON.stringify(res, undefined, 2));
+      $("#api-champion-output").text(JSON.stringify(res, undefined, 2));
       UpdateApiDemoOutput(url);
-    $("#api-champions-name").prop("selectedIndex", 0);
+      $("#api-champion-names").prop("selectedIndex", 0); // Reset champion selector
+      $("#api-champion-cost").val('').trigger("chosen:updated"); // Clear filtered costs
+      $("#api-champion-traits").val('').trigger("chosen:updated"); // Clear filtered traits
     });
   });
 
   // When trait is chosen to be filtered
-  $("#api-champions-traits").chosen().change(() => {
+  $("#api-champion-traits").chosen().change(() => {
     FilterChampionsUsingApi();
-    $("#api-champions-name").prop("selectedIndex", 0);
+    $("#api-champion-names").prop("selectedIndex", 0); // Reset champion selector
   });
 
   // When cost is chosen to be filtered 
-  $("#api-champions-cost").chosen().change(() => {
+  $("#api-champion-cost").chosen().change(() => {
     FilterChampionsUsingApi();
-    $("#api-champions-name").prop("selectedIndex", 0);
+    $("#api-champion-names").prop("selectedIndex", 0); // Reset champion selector
   });
 
   // -- Traits API --
   // When trait is selected
-  $("#api-traits-name").change((event) => {
-    let traitName = $("#api-traits-name").val();
+  $("#api-trait-names").change((event) => {
+    let traitName = $("#api-trait-names").val();
     let url = GetDataApiUrl(GetCurrentSet(), "traits", {"name": traitName});
     Fetch(url).then((res) => {
-      $("#api-traits-output").text(JSON.stringify(res, undefined, 2));
+      $("#api-trait-output").text(JSON.stringify(res, undefined, 2));
       UpdateApiDemoOutput(url);
+      $("#api-trait-types").val('').trigger("chosen:updated"); // Clear filtered trait types
     });
   });
 
   // When "All" button is clicked
-  $("#api-traits-all").click(() => {
+  $("#api-trait-all").click(() => {
     let url = GetDataApiUrl(GetCurrentSet(), "traits");
     Fetch(url).then((res) => {
-      $("#api-traits-output").text(JSON.stringify(res, undefined, 2));
+      $("#api-trait-output").text(JSON.stringify(res, undefined, 2));
       UpdateApiDemoOutput(url);
-      $("#api-traits-name").prop("selectedIndex", 0);
+      $("#api-trait-names").prop("selectedIndex", 0); // Reset trait selector
+      $("#api-trait-types").val('').trigger("chosen:updated"); // Clear filtered trait types
     });
   });
 
   // When trait type is chosen to be filtered 
-  $("#api-traits-types").chosen().change(() => {
-    let typesString = $("#api-traits-types").chosen().val();
+  $("#api-trait-types").chosen().change(() => {
+    let typesString = $("#api-trait-types").chosen().val();
     let filter = {type: typesString.join(' ').toLowerCase()};
     let url = GetDataApiUrl(GetCurrentSet(), "traits", filter);
     Fetch(url).then((res) => {
-      $("#api-traits-output").text(JSON.stringify(res, undefined, 2));
+      $("#api-trait-output").text(JSON.stringify(res, undefined, 2));
       UpdateApiDemoOutput(url);
-      $("#api-traits-name").prop("selectedIndex", 0);
-
+      $("#api-trait-names").prop("selectedIndex", 0); // Reset trait selector
     });
   });
  
   // -- Item API --
-  // When item query selector is changed
-  $("#api-items-query").change((event) => {
+  // When item query (name/ID) is selected
+  $("#api-item-query").change((event) => {
     UpdateItemSelectorValues(); // Names/IDs
+    $("#api-item-output").text(''); // Reset item output
   });
 
-  //TODO: item selector change event
-  $("#api-items-select").keypress((event) => {
-    if (event.key === "Enter") {
-      let itemQuery = $("#api-items-query").val()
-      let itemText = $("#api-items-select").val();
-      let filter = {};
-      if (itemQuery === "ID") {
-        filter["id"] = itemText;
-      }
-      else if (itemQuery === "Name") {
-        filter["name"] = itemText;
-      }
-      let url = GetDataApiUrl(GetCurrentSet(), "items", filter);
-      Fetch(url).then((res) => {
-        $("#api-items-output").text(JSON.stringify(res, undefined, 2));
-        UpdateApiDemoOutput(url);
-      });
+  // When item name/ID is selected
+  $("#api-item-select").change((event) => {
+    let filter = {};
+    if ($("#api-item-query").val() == "ID") {
+      filter["id"] = $("#api-item-select").val();
     }
-  });
-
-  // When item attribute is chosen to be filtered
-  $("#api-items-attributes").chosen().change(() => {
-    let itemString = $("#api-items-attributes").chosen().val();
-    let filter = {attr: itemString.join(' ').toLowerCase()};
+    else {
+      filter["name"] = $("#api-item-select").val();
+    }
     let url = GetDataApiUrl(GetCurrentSet(), "items", filter);
     Fetch(url).then((res) => {
-      $("#api-items-output").text(JSON.stringify(res, undefined, 2));
+      $("#api-item-output").text(JSON.stringify(res, undefined, 2));
       UpdateApiDemoOutput(url);
-      //TODO item selector to index 0
+      $("#api-item-attributes").val('').trigger("chosen:updated"); // Clear item attributes
     });
   });
 
-  //TODO: "All" item button
-  $("#api-items-submit").click(() => {
-    let itemQuery = $("#api-items-query").val()
-    let itemText = $("#api-items-select").val();
-    let filter = {};
-    if (itemQuery === "ID") {
-      filter["id"] = itemText;
-    }
-    else if (itemQuery === "Name") {
-      filter["name"] = itemText;
-    }
+  // When "All" button is clicked
+  $("#api-item-all").click(() => {
+    let url = GetDataApiUrl(GetCurrentSet(), "items");
+    Fetch(url).then((res) => {
+      $("#api-item-output").text(JSON.stringify(res, undefined, 2));
+      UpdateApiDemoOutput(url);
+      $("#api-item-select").prop("selectedIndex", 0); // Reset item selector
+      $("#api-item-attributes").val('').trigger("chosen:updated"); // Clear item attributes
+    });
+  });
+
+  // When item attribute is chosen to be filtered
+  $("#api-item-attributes").chosen().change(() => {
+    let itemString = $("#api-item-attributes").chosen().val();
+    let filter = {attr: itemString.join(' ').toLowerCase()};
     let url = GetDataApiUrl(GetCurrentSet(), "items", filter);
     Fetch(url).then((res) => {
-      $("#api-items-output").text(JSON.stringify(res, undefined, 2));
+      $("#api-item-output").text(JSON.stringify(res, undefined, 2));
       UpdateApiDemoOutput(url);
-      //TODO item selector to index 0
+      $("#api-item-select").prop("selectedIndex", 0); // Reset item selector
     });
   });
 
   // -- Image API --
-  //TODO: remove this
-  $("#api-img-submit").click(() => {
-    UpdateApiImage();
+  // When image query (champion/trait/item) is selected
+  $("#api-img-query").change((event) => {
+    UpdateImageSelectorValues();
+    $("#api-img-result").attr("src", ''); // Reset image output
   });
 
-  //TODO: image selector
-  $("#api-img-text").keypress((event) => {
-    if (event.key === "Enter") {
-      UpdateApiImage();
-    }
+  // When name of champion/trait/item is selected
+  $("#api-img-select").change((event) => {
+    UpdateApiImage();
   });
 }
 
